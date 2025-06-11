76571a921354a8315f3285e4f350820a9208454f
Denis <denis30volovic@gmail.com>
2025-06-11 20:36:37 +0300

    Added docs

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 /**
  * @description Define API routes
  */
-app.use('/api', require('./routes/userRouter'));
+app.use('/api', require('./routes/UserRouter'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

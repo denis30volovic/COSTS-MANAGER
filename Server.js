c1571ae2d2a81412d2b926b11d1709d2d4eea9d6
Denis <denis30volovic@gmail.com>
2025-06-11 21:34:25 +0300

    Refactor server.js to follow naming conventions

--- Server.js
+++ Server.js
@@ -37,7 +37,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 /**
  * @description Define API routes
  */
-app.use('/api', require('./routes/UserRouter'));
+app.use('/api', require('./routes/userRouter'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
